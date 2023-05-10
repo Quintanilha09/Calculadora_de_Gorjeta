dba266706711c7a87658c09754cb03de1ba89b49
EA - Gabriel Quintanilha Cabral De Oliveira <gabriel_quintanilha@educadventista.org>
2023-05-09 21:30:58 -0300

    commit2

--- calculadora-de-gorjeta/src/components/Calculadora.jsx
+++ calculadora-de-gorjeta/src/components/Calculadora.jsx
@@ -1,25 +1,53 @@
 export function Calculadora() {
     return(
-        <form action="">
-            <div className="bill">
-                <label htmlFor="">Conta</label>
-                <div className="input-box">
-                    <input type="text"/>
-                </div>
+       <main>
+            <form action="">
+                <div className="bill">
+                    <label htmlFor="bill">Conta</label>
+                    <div className="input-box">
+                        <input type="number" id="bill" placeholder="0"/>
+                    </div>
 
-                <div className="tip-percentage">
-                    <label htmlFor="">Selecione a porcentagem</label>
-                    <div>
-                        <input type="butoon" value="5%" />
-                        <input type="butoon" value="10%" />
-                        <input type="butoon" value="15%" />
-                        <input type="butoon" value="25%" />
-                        <input type="butoon" value="50%" />
+                    <div className="tip-percentage">
+                        <label htmlFor="custom">Selecione a porcentagem</label>
+                        <div>
+                            <input type="button" value="5%" />
+                            <input type="button" value="10%" />
+                            <input type="button" value="15%" />
+                            <input type="button" value="25%" />
+                            <input type="button" value="50%" />
 
-                        <input type="number" id="custom" placeholder="Outro" />
+                            <input type="number" id="custom" placeholder="Outra" />
+                        </div>
+
+                        <div className="people">
+                            <label htmlFor="number">Número de Pessoas</label>
+                            <div className="input-box">
+                                <input type="number" name="people-number" placeholder="0" />
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
+            
+            <section className="results">
+                <div className="tip-amount">
+                    <p>
+                        Gorjeta <br />
+                        <span>/ pessoa</span>
+                    </p>
+                    <p>
+                        R$0.00
+                    </p>
+                </div>
+
+                <div className="total">
+                    <p>Total <br /></p>
+                    <p>R$0.00</p>
+                </div>
+
+                <button>Limpar</button>
+            </section>
+       </main>
     )
 }
